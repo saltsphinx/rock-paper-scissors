c2f6c99e4a13859410d913a8a7baf85027607df6
tremar thomas <tremar.thomas19@gmail.com>
2022-06-20 23:22:19 -0500

    Create file structure, finish computer and userPlay functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+write a program that allows the user to play rock paper scissors against the computer
+  write a function called computerPlay that plays a random hand
+  write a function called userPlay that gets user input and parses it
+  write a function that accepts the results of the two above functions and determines a winner
---
+++ assets/script.js
@@ -0,0 +1,22 @@
+let hands = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+  return hands[Math.floor(Math.random() * 3 + 1)];
+}
+
+function userPlay() {
+  let userChoice;
+  let result;
+
+  console.log('Choose a hand: Rock, paper or scissors.');
+  while (typeof userChoice != 'string') {
+    if (result) { console.log('You have to choose between rock, paper or scissors'); }
+
+    result = window.prompt().toLowerCase().trim();
+    if (hands.includes(result)) { userChoice = result; }
+  }
+
+  function playRound(computerHand, userHand) {
+    
+  }
+}
